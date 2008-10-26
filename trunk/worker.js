dd91cc164bd76eaeffa63e0675f3993befd074c0
beshkenadze <beshkenadze@0b8ae620-a35e-11dd-a887-7ffe1a420f8d>
2008-10-26 13:09:20 +0000

    init project

---
+++ trunk/worker.js
@@ -0,0 +1,5 @@
+var wp = google.gears.workerPool;
+wp.onmessage = function(a, b, message) {
+  var reply = message.body[0] + message.body[1] + "... " + message.body[2].helloWorld;
+  wp.sendMessage(reply, message.sender);
+}
